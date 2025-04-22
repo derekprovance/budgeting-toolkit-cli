8ac049c1114cc19b936b525163a7f3db46234ca3
Derek Provance <me@derek.pro>
2025-04-21 21:39:24 -0600

    Increment Version

--- src/cli.ts
+++ src/cli.ts
@@ -85,7 +85,7 @@ export const createCli = (): Command => {
   program
     .name("budgeting-toolkit-cli")
     .description("CLI to perform budgeting operations with Firefly III API")
-    .version("2.3.3");
+    .version("2.3.4");
 
   program
     .command("finalize-budget")
